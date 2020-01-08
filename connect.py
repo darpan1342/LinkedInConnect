502cc5d73545ccce2b5cd325c810d9d8bdea237c
BurnedQuater <36494771+darpan1342@users.noreply.github.com>
2020-01-08 12:23:00 +0530

    Add files via upload

---
+++ connect.py
@@ -0,0 +1,12 @@
+import urllib.request,urllib.error,urllib.parse
+import ssl
+import bs4
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+shandle = urllib.request.urlopen('https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin', context=ctx )
+
+for i in shandle:
+    print(i.decode())
